e1174cfa8bd2e0ed70c66882a91ba2568e72c411
MrDoLoop <nilaile8@gmail.com>
2013-11-05 11:12:16 +0100

    tttt

--- src/com/doloop/www/MainActivity.java
+++ src/com/doloop/www/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends SlidingFragmentActivity implements
 	private final static int SYS_APPS_TAB_POS = 1;
 	private final static int ALL_APPS_TAB_POS = 2;
 	
-	private final static int ALL_APPS_TAB_POS111 = 2;
+
 
 	private ActionBar actionBar;
 	private ViewPager viewPager;
