a7a0c21a2aae8d663d28fdb215783f37fc746d72
MrDoLoop <nilaile8@gmail.com>
2013-11-12 14:26:09 +0100

    ttt

--- src/com/doloop/www/util/SysAppListAdapter.java
+++ src/com/doloop/www/util/SysAppListAdapter.java
@@ -1,9 +1,7 @@
 package com.doloop.www.util;
 
-import java.text.Collator;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Locale;
